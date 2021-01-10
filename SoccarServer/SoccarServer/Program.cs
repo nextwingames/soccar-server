98272516b62689458e1c5095c40dfa4390536431
이영재 <youngjewall@me.com>
2021-01-10 18:27:03 +0900

    Make VisualStudio Project

---
+++ SoccarServer/SoccarServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SoccarServer
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
